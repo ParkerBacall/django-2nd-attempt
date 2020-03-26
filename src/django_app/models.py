386381c4e9d993d7f1b4f4708b56b4bab2b7b913
ParkerBacall <pbacall2@gmail.com>
2020-03-26 14:37:32 -0600

    refactor

--- src/django_app/models.py
+++ src/django_app/models.py
@@ -6,6 +6,3 @@ from django.db import models
 class Bear(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
-
-    def __str__(self):
-        return self.name
